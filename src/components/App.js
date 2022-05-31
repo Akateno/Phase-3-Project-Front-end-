b62ae87c3a9f3248f90aa482a7efaf624a1c1840
Erick Vargas <erickkvargas@gmail.com>
2022-05-31 15:20:57 -0700

    App component edit

--- src/components/App.js
+++ src/components/App.js
@@ -1,57 +1,60 @@
 import React, { useEffect, useState } from "react";
-import Header from "./Header";
-import Search from "./Search";
-import MessageList from "./MessageList";
-import NewMessage from "./NewMessage";
+import DogContainer from "./DogContainer"
+import NavBar from "./NavBar"
+import ApplicantPage from "./ApplicantPage"
 
-const testUser = { username: "Duane" };
+
+
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(true);
-  const [messages, setMessages] = useState([]);
-  const [search, setSearch] = useState("");
+  
+  const [dogs, setDogs] = useState([]);
+  const [applicant, setApplicants] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:4000/messages")
+    fetch("http://localhost:9292/dogs")
       .then((r) => r.json())
-      .then((messages) => setMessages(messages));
+      .then((dogs) => setDogs(dogs));
   }, []);
 
-  function handleAddMessage(newMessage) {
-    setMessages([...messages, newMessage]);
+  useEffect(() => {
+    fetch("http://localhost:9292/applicants")
+      .then((r) => r.json())
+      .then((applicants) => setDogs(applicants));
+  }, []);
+
+
+  function handleDogs(newDog) {
+    setDogs([...dogs, newDog]);
   }
 
-  function handleDeleteMessage(id) {
-    const updatedMessages = messages.filter((message) => message.id !== id);
-    setMessages(updatedMessages);
-  }
+  // function handleDeleteMessage(id) {
+  //   const updatedMessages = messages.filter((message) => message.id !== id);
+  //   setMessages(updatedMessages);
+  // }
 
-  function handleUpdateMessage(updatedMessageObj) {
-    const updatedMessages = messages.map((message) => {
-      if (message.id === updatedMessageObj.id) {
-        return updatedMessageObj;
-      } else {
-        return message;
-      }
-    });
-    setMessages(updatedMessages);
-  }
+  // function handleUpdateMessage(updatedMessageObj) {
+  //   const updatedMessages = messages.map((message) => {
+  //     if (message.id === updatedMessageObj.id) {
+  //       return updatedMessageObj;
+  //     } else {
+  //       return message;
+  //     }
+  //   });
+  //   setMessages(updatedMessages);
+  // }
 
-  const displayedMessages = messages.filter((message) =>
-    message.body.toLowerCase().includes(search.toLowerCase())
-  );
+  // const displayedMessages = messages.filter((message) =>
+  //   message.body.toLowerCase().includes(search.toLowerCase())
+  // );
 
   return (
-    <main className={isDarkMode ? "dark-mode" : ""}>
-      <Header isDarkMode={isDarkMode} onToggleDarkMode={setIsDarkMode} />
-      <Search search={search} onSearchChange={setSearch} />
-      <MessageList
-        messages={displayedMessages}
-        currentUser={testUser}
-        onMessageDelete={handleDeleteMessage}
-        onUpdateMessage={handleUpdateMessage}
-      />
-      <NewMessage currentUser={testUser} onAddMessage={handleAddMessage} />
+    <main>
+     <DogContainer/>
+     <NavBar/>
+     <ApplicantPage/>
+
+
     </main>
   );
 }
