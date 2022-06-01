b84e0fc70c7276007ba32b2b82022aea919ba198
Daven Young <davenyoung@msn.com>
2022-06-01 10:29:39 -0600

    added form and start of ApplicantPage

--- src/components/ApplicantPage.js
+++ src/components/ApplicantPage.js
@@ -1,57 +1,18 @@
 import React, { useState } from "react";
-import EditMessage from "./EditMessage";
 
-function Message({ message, currentUser, onMessageDelete, onUpdateMessage }) {
-  const [isEditing, setIsEditing] = useState(false);
 
-  const { id, username, body, created_at: createdAt } = message;
-
-  const timestamp = new Date(createdAt).toLocaleTimeString();
-
-  const isCurrentUser = currentUser.username === username;
-
-  function handleDeleteClick() {
-    fetch(`http://localhost:4000/messages/${id}`, {
-      method: "DELETE",
-    });
-
-    onMessageDelete(id);
-  }
-
-  function handleUpdateMessage(updatedMessage) {
-    setIsEditing(false);
-    onUpdateMessage(updatedMessage);
-  }
-
-  return (
-    <li>
-      <span className="user">{username}</span>
-      <span className="time">{timestamp}</span>
-      {isEditing ? (
-        <EditMessage
-          id={id}
-          body={body}
-          onUpdateMessage={handleUpdateMessage}
-        />
-      ) : (
-        <p>{body}</p>
-      )}
-      {isCurrentUser ? (
-        <div className="actions">
-          <button onClick={() => setIsEditing((isEditing) => !isEditing)}>
-            <span role="img" aria-label="edit">
-              ✏️
-            </span>
-          </button>
-          <button onClick={handleDeleteClick}>
-            <span role="img" aria-label="delete">
-              🗑
-            </span>
-          </button>
-        </div>
-      ) : null}
-    </li>
-  );
+function ApplicantPage( ) {
+  <div>
+    <h1>{name}</h1>
+    <p>{job}</p>
+    <p>{city}</p>
+    <p>{pets_owned}</p>
+    <p>{pet_id}</p>
+  </div>,
+  <div>
+    <h2>applicants</h2>
+    <p>{applicants}</p>
+  </div>
 }
 
-export default Message;
+export default ApplicantPage;
